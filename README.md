67081ece7f5a622a5e2aa4754b6ffe6e26d16670
Peter Kazarinoff <peter.kazarinoff@pcc.edu>
2021-05-28 11:04:36 -0700

    working on Fast API dev

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # fast-api
 
 An example Fast API web API. Just a sample project, not for prouduction or a full website backend.
+
+## Installation
+
+```
+# python==3.9.5
+python -m venv venv
+source venv/bin/activate
+python -m pip install -r requirements.txt
+```
+
+## Run
+
+```
+uvicorn main:api --reload
+# http://localhost:8000/
+# http://localhost:8000/api/calculate?x=7&y=2
+# Ctrl-c to stop
+
+uvicorn main2:app
+# http://localhost:8000/
+# http://localhost:8000/items/2
+
+
+uvicorn template-app:app
+# http://localhost:8000/items/2
+```
--- main.py
+++ main.py
@@ -2,16 +2,35 @@
 
 from typing import Optional
 
-from fastapi import FastAPI
+import fastapi
+import uvicorn
 
-app = FastAPI()
-
-
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+api = fastapi.FastAPI()
 
 
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@api.get("/")
+async def index():
+    body = (
+        "<html>"
+        "<body style='padding: 10px;'>"
+        "<div>"
+        "Try it <a href='/api/calculate?x=7&y=2'>/api/calculate?x=7&y=2</a>"
+        "</div>"
+        "</body>"
+        "</html"
+    )
+    return fastapi.responses.HTMLResponse(content=body)
+
+
+@api.get("/api/calculate")
+async def calculate(x: int, y: int, z: Optional[int] = None):
+    if z == 0:
+        return fastapi.responses.JSONResponse(
+            content={"error": "Error: Z can not be zero"}, status_code=400
+        )
+
+    value = x + y
+    if z is not None:
+        value = value / z
+
+    return {"x": x, "y": y, "z": z, "value": value}
